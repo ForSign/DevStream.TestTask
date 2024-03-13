5d47ee9bae773499a780c9f17c59c4c2f436e599
ForSign <tim.shiryaev@gmail.com>
2024-03-14 02:01:26 +0300

    add download function to DownloadButton

--- JS/react_sap/src/components/UI/Button/DownloadButton.jsx
+++ JS/react_sap/src/components/UI/Button/DownloadButton.jsx
@@ -3,9 +3,22 @@ import cl from './DownloadButton.module.css';
 
 const DownloadButton = ({href}) => {
 
+    async function download() {
+        const image = await fetch(href)
+        const imageBlog = await image.blob()
+        const imageURL = URL.createObjectURL(imageBlog)
+
+        const link = document.createElement('a')
+        link.href = imageURL
+        link.download = href.split('/').pop() + Date.now();
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
+
     return (
         <div className={cl.button}>
-            <a href={href} className="btn-wrap">Download</a>
+            <a className="btn-wrap" onClick={download}>Download</a>
         </div>
     );
 };
